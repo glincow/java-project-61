33d9e4759e352261769f76305a641847a05685ed
Anatolii Siuniaev <a.a.syunyaev@gmail.com>
2025-02-18 14:31:53 +0300

    Fixed style

--- app/src/main/java/hexlet/code/games/Game.java
+++ app/src/main/java/hexlet/code/games/Game.java
@@ -1,11 +1,7 @@
 package hexlet.code.games;
 
-import hexlet.code.Cli;
-
-import java.util.Scanner;
-
 public interface Game {
-    static final int MAX_ROUNDS_COUNT = 3;
+    int MAX_ROUNDS_COUNT = 3;
 
     String[] generateQuestionAndAnswer();
 
